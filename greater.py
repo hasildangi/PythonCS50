4c1ecd77abe9ad3dd1da5ccb860768aaae7e1b94
Lokindra Dangi <pachar.com@gmail.com>
2023-08-02 07:58:55 +0545

    And conditionals

---
+++ greater.py
@@ -0,0 +1,12 @@
+a = int(input("Enter the Fist Value"))
+b = int(input("Enter the Second Value"))
+c = int(input("Enter the Third Value "))
+
+if a > b and a > c :
+    print(" a is the greatest")
+
+elif b > c and b > a:
+    print("b  is the greatest")
+
+elif c > a and c > b:
+    print("c is the greatest") 
--- useof OR.py
+++
@@ -1,8 +0,0 @@
-a = input("Enter the Fist Value")
-b = input("Enter the Second Value")
-
-if a > b or a < b :
-    print(" a is not equal to b")
-else:
-    print("a is equal to b")
-
