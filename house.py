9f5bf2719918e268be05ceff1209e68777657953
Lokindra Dangi <pachar.com@gmail.com>
2023-08-06 08:27:29 +0545

    match case

--- house.py
+++ house.py
@@ -1,9 +1,12 @@
+#match shortcut 
 name = input("what's your name?") 
 
 match name:
-    case"Hasil":
+    case"Hasil" |"Lokindra" | "Razzi":
         print("Shantinagar")
     case"Kamal":
         print("chinna")
     case"Kushal":
         print("kullalbada")
+    case _:
+        print ("who are you?")
