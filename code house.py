199746b52ee4b85e43d53bb2558e982eadcaca92
Lokindra Dangi <pachar.com@gmail.com>
2023-08-06 08:10:01 +0545

    code house

---
+++ code house.py
@@ -0,0 +1,13 @@
+x = input("whst's your name?")
+if x == "Hasil":
+        print("shantinagar")
+elif x == "Kushal":
+        print("Humla")
+elif  x == "Kamal":
+        print("chhinna")
+
+else:
+        print("Who?")
+        
+
+
