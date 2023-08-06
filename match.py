667be7e8002b584cc3635b13cca31a5fcd04bdc9
Lokindra Dangi <pachar.com@gmail.com>
2023-08-06 08:16:40 +0545

    match case

---
+++ match.py
@@ -0,0 +1,9 @@
+name = input("what's your name?") 
+
+match name:
+    case"Hasil":
+        print("Shantinagar")
+    case"Kamal":
+        print("chinna")
+    case"Kushal":
+        print("kullalbada")
